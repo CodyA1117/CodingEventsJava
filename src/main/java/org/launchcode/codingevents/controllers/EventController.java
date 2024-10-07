b8dd63def395b43d828d745df0060264f9d5375c
CodyA1117 <adamscody079@gmail.com>
2024-10-07 10:25:46 -0500

    Completed Ch13 reading practice from videos.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,9 +1,12 @@
 package org.launchcode.codingevents.controllers;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,15 +18,28 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
-        model.addAttribute("events", events);
+       model.addAttribute("events", events);
+       model.addAttribute("title","All Events");
         return "events/index";
+    }
+
+    // lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(Model model){
+        model.addAttribute("title","Create Event");
+        return "events/create";
+    }
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:/events";
+
+
     }
 
 }
