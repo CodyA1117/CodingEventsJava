cf70a8c3234889810b38ed1fecb036b2d82fe26d
CodyA1117 <adamscody079@gmail.com>
2024-10-07 11:16:45 -0500

    Completed exercises for chapter 13.  Still working on dynamic image loading, but got the static images to display.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,45 +1,43 @@
 package org.launchcode.codingevents.controllers;
 
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
-/**
- * Created by Chris Bay
- */
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
+
+    // Adding initial events with descriptions
+    static {
+        events.put("Menteaship", "A fun meetup for connecting with mentors");
+        events.put("Code With Pride", "A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
+    }
 
     @GetMapping
     public String displayAllEvents(Model model) {
-       model.addAttribute("events", events);
-       model.addAttribute("title","All Events");
+        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
         return "events/index";
     }
 
-    // lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm(Model model){
-        model.addAttribute("title","Create Event");
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
-    //lives at /events/create
+
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
+        events.put(eventName, eventDescription);
         return "redirect:/events";
-
-
     }
-
 }
